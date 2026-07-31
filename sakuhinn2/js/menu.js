bcaa68fb109e68be18d8ec3194b35cc53526d189
emi1088 <855c89zk8d@privaterelay.appleid.com>
2026-07-31 09:54:31 +0900

    Delete sakuhinn2 directory

--- sakuhinn2/js/menu.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.getElementById("nav");
-
-hamburger.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
